37964887ec5361d3f6021c1814a5df52e0f52b0d
Gianluca Casati <fibo@users.noreply.github.com>
2023-11-04 23:28:51 +0100

    use polygon contract

--- frontend/src/contractAddress.js
+++ frontend/src/contractAddress.js
@@ -1,5 +1,5 @@
 // Polygon ZK EVM
-// export const contractAddress = "0x711aba8597ec3a47d375cd5391d04c904a0e60bf";
+export const contractAddress = "0xCB75125ff92dC958140dEFC2E705461E6b2C938c";
 
 // Goerli
-export const contractAddress = "0x256620f695aaaa357abd49fb2832b6c2945d1db4";
+// export const contractAddress = "0x256620f695aaaa357abd49fb2832b6c2945d1db4";
