dd71ec35b126175e6e2f1db9f0da1a7d0c68ef63
IreneB <46067606+IreneBa26@users.noreply.github.com>
2023-11-05 02:57:27 +0000

    Update web3FA.sol
    Working contract deployed on zkEVM at address:
    
    - 0xCB75125ff92dC958140dEFC2E705461E6b2C938c

--- contract/web3FA.sol
+++ contract/web3FA.sol
@@ -1,3 +1,7 @@
+/**
+ *Submitted for verification at testnet-zkevm.polygonscan.com on 2023-11-04
+*/
+
 // SPDX-License-Identifier: GPL-3.0
 
 pragma solidity ^0.8.0;
@@ -5,13 +9,47 @@ pragma solidity ^0.8.0;
 contract Web3FA {
 
     mapping(address => string) private userStrings;
+    mapping(address => address) private delegates;
 
-    function write(string memory newString) public {
-            require(msg.sender == tx.origin, "You can only write from your address");
-            userStrings[msg.sender] = newString;
+
+    // The delegateAccess function allows msg.sender to delegate access to another address (delegate)
+    function delegateAccess(address delegate) public {
+        require(msg.sender == tx.origin, "You can only delegate from your address");
+
+        // Set the delegate for the message sender
+        delegates[msg.sender] = delegate;
     }
 
+    // The revokeAccess function revokes the access by setting the delegate address to zero (address(0))
+    function revokeAccess() public {
+        // Revoke the access by setting the delegate to zero address
+        delegates[msg.sender] = address(0);
+    }
+
+    // The write and read functions check if msg.sender has set a delegate. 
+    // If a delegate is set, operations are performed on the delegate's account. Otherwise, the operations are executed on the msg.sender account.
+
+    function write(string memory newString) public {
+        address delegate = delegates[msg.sender];
+
+        if (delegate != address(0)) {
+            userStrings[delegate] = newString;
+        } else {
+            require(msg.sender == tx.origin, "You can only write from your address");
+            userStrings[msg.sender] = newString;
+        }
+    }
+
+
     function read(address userAddress) public view returns (string memory) {
+        require(msg.sender == userAddress, "You can only read your own data");
+        
+        address delegate = delegates[userAddress];
+
+        if (delegate != address(0)) {
+            return userStrings[delegate];
+        } else {
             return userStrings[userAddress];
+        }
     }
 }
