cd8fb3b0dc218e4845cc294d45424f53a07e097f
Gianluca Casati <fibo@users.noreply.github.com>
2023-11-05 04:10:15 +0000

    add neon

--- frontend/src/contractAddress.js
+++ frontend/src/contractAddress.js
@@ -1,5 +1,8 @@
 // Polygon ZK EVM
-export const contractAddress = "0xCB75125ff92dC958140dEFC2E705461E6b2C938c";
+// export const contractAddress = "0xCB75125ff92dC958140dEFC2E705461E6b2C938c";
 
 // Goerli
 // export const contractAddress = "0x256620f695aaaa357abd49fb2832b6c2945d1db4";
+
+// Neon
+export const contractAddress = "0x510078fb6e7c9ce56a2cc6f294fcd6405b777c74";
